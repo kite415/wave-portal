436f9df233cdca59af2eeaf398b1cf2317b1a624
menaceonzenith <63975770+menaceonzenith@users.noreply.github.com>
2022-04-15 21:04:41 -0400

    tested own wave

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,22 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+
+    uint64 totalWaves;
+
     constructor() {
         console.log("first contract, what's good");
+    }
+
+    // when wave() is called, it adds one wave to our total count
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved", msg.sender);
+    }
+
+    // gets us the total wave count
+    function getTotalWaves() public view returns (uint64) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
